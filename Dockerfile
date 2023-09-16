520bec11ee05c287f40c0a4605b66e8df053352e
Harsha Gunawardane <mrgunawardaneofficial@gmail.com>
2023-09-16 11:35:27 +0530

    Connect with mongoDB

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,16 @@ WORKDIR /app
 COPY package.json .
 # install packages
 RUN npm install
+
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development" ]; \
+        then npm install; \
+        else npm install --only=production; \
+        fi
+        
 # then copy other stuff
 COPY . ./
 ENV PORT 3000
 EXPOSE $PORT
 # command for run the container
-CMD ["npm", "run", "dev"]
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
+
 const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Successfully connected"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
-  res.send("<H2>Hello docker..</H2>");
+  res.send("<H2>Hello docker....</H2>");
 });
 
 const port = process.env.PORT || 3000;
